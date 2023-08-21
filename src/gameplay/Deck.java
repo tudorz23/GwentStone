d4762591a8c9833b00aa4279f690ec45adb83ec9
Tudor Zaharia <zahariatm@yahoo.com>
2023-08-21 14:53:29 +0300

    Create deep copy methods for Card and Deck.

--- src/gameplay/Deck.java
+++ src/gameplay/Deck.java
@@ -2,6 +2,8 @@ package gameplay;
 
 import java.util.ArrayList;
 import cards.Card;
+import cards.environment.*;
+import cards.minion.*;
 
 /**
  * Contains the cards that form a deck.
@@ -10,11 +12,64 @@ public class Deck {
     private ArrayList<Card> cardSet;
     private int nrCards;
 
+    /* Constructor */
     public Deck(int nrCards) {
         this.cardSet = new ArrayList<>();
         this.nrCards = nrCards;
     }
 
+    /**
+     * Only need to copy Minion and Environment Cards.
+     * Used only in deckDeepCopy() method, for the start of a mini-game.
+     * @param card Card of type Minion or Environment
+     * @return deep copy of the card
+     */
+    private Card cardDeepCopy(Card card) {
+        // Create deep copies of common fields
+        String copyDescription = new String(card.getDescription());
+
+        ArrayList<String> copyColors = new ArrayList<>();
+        for (String color : card.getColors()) {
+            String copyOneColor = new String(color);
+            copyColors.add(copyOneColor);
+        }
+
+        String copyName = new String(card.getName());
+
+        return switch (card.getName()) {
+            case "The Ripper" -> new TheRipper(card.getMana(), copyDescription, copyColors,
+                    copyName, ((Minion) card).getHealth(), ((Minion) card).getAttack());
+            case "Miraj" -> new Miraj(card.getMana(), copyDescription, copyColors,
+                    copyName, ((Minion) card).getHealth(), ((Minion) card).getAttack());
+            case "The Cursed One" -> new TheCursedOne(card.getMana(), copyDescription, copyColors,
+                    copyName, ((Minion) card).getHealth(), ((Minion) card).getAttack());
+            case "Disciple" -> new Disciple(card.getMana(), copyDescription, copyColors,
+                    copyName, ((Minion) card).getHealth(), ((Minion) card).getAttack());
+            case "Firestorm" -> new Firestorm(card.getMana(), copyDescription, copyColors, copyName);
+            case "Winterfell" -> new Winterfell(card.getMana(), copyDescription, copyColors, copyName);
+            default -> new HeartHound(card.getMana(), copyDescription, copyColors, copyName);
+        };
+    }
+
+    /**
+     * Creates a deep Copy for a deck from the player's packDeck.
+     * @param deck deck that must be copied
+     * @return deep copy of the deck
+     */
+    public Deck deckDeepCopy(Deck deck) {
+        Deck copyDeck = new Deck(deck.nrCards);
+        copyDeck.cardSet = new ArrayList<>();
+
+        // Deep copy each of the cards.
+        for (Card card : deck.getCardSet()) {
+            Card copyCard = cardDeepCopy(card);
+            cardSet.add(copyCard);
+        }
+
+        return copyDeck;
+    }
+
+    /* Getters and Setters */
     public void setCardSet(ArrayList<Card> cardSet) {
         this.cardSet = cardSet;
     }
--- src/gameplay/Game.java
+++ src/gameplay/Game.java
@@ -3,20 +3,28 @@ package gameplay;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import fileio.Input;
+import fileio.StartGameInput;
 import utils.Converter;
 
 public class Game {
     private Player player1;
     private Player player2;
     public Input input;
-    public ArrayNode output = (new ObjectMapper()).createArrayNode();
+    public ArrayNode output;
+    // Changes from one mini-game to the other
+    private int shuffleSeed;
 
+    /* Constructor */
     public Game(Input input) {
         this.player1 = new Player();
         this.player2 = new Player();
         this.input = input;
+        this.output = (new ObjectMapper()).createArrayNode();
     }
 
+    /**
+     * Completes the packDeck field for both Player objects.
+     */
     public void parsePlayerDecks() {
         Converter converter = new Converter();
         player1.setPackDeck(converter.convertDeck(input.getPlayerOneDecks()));
@@ -26,6 +34,29 @@ public class Game {
         player2.setNrDecks(input.getPlayerTwoDecks().getNrDecks());
     }
 
+    /**
+     * Sets the current deck for each player.
+     * Sets the Hero for each player.
+     * Selects the shuffleSeed.
+     * @param miniGame returned from a loop that iterates through all the mini-games
+     */
+    public void prepareMiniGame(StartGameInput miniGame) {
+        // Set the shuffleSeed
+        this.shuffleSeed = miniGame.getShuffleSeed();
+
+        // Set the heroes
+        Converter converter = new Converter();
+        player1.setHero(converter.convertHero(miniGame.getPlayerOneHero()));
+        player2.setHero(converter.convertHero(miniGame.getPlayerTwoHero()));
+
+
+    }
+
+
+
+
+
+    /* Getters and Setters*/
     public Player getPlayer1() {
         return player1;
     }
